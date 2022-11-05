4a6dcbeadd470b5bedc72da1c326124ebc402141
Tansil011019 <16521295@std.stei.itb.ac.id>
2022-11-05 18:30:35 +0700

    cam and parser

--- src/data/configdata.py
+++ src/data/configdata.py
@@ -1,13 +1,17 @@
+import os
 import cv2 as cv
 import glob
+import time
 
-# ImagePath= 'D:\\School\\Institut Teknologi Bandung\\Tahun 2\\Semester 1\\Linear Algebra dan Geometri\\Tugas Besar 2\\Algeo02-21054\\test\\pins_Adriana\\*jpg'
+# ImagePath= 'D:\\School\\Institut Teknologi Bandung\\Tahun 2\\Semester 1\\Linear Algebra dan Geometri\\Tugas Besar 2\\Algeo02-21054\\test\\pins_Adriana\\*.jpg'
+ImagePath= './test/pins_Adriana/*.jpg'
 
 cv_img = []
 int_img= []
 
 def Parser(path):
-    DirPath = path
+    DirPath = os.path.abspath(path)
+    print(DirPath)
     File= glob.glob(DirPath)
     for file in File:
         img= cv.imread(file)
@@ -16,6 +20,32 @@ def Parser(path):
         grayscale_img= cv.cvtColor(img_resize, cv.COLOR_BGR2GRAY)
         int_img.append(grayscale_img)
 
+def camera_use():
+    cam= cv.VideoCapture(0) #Start Camera
+    cv.namedWindow("Camera") #Windows Title
+    img_counter= 0
+    while True:
+        time.sleep(10)# Pausing for 10s
+        ret, frame= cam.read()
+        if(not ret):
+            print("Failed to proccess..")
+            break
+        cv.imshow("Display", frame) #Showing camera
+        k= cv.waitKey(1) #Key
+        if k%256 == 27:
+            print("Closing camera..") #Key == Esc
+            break
+        img_name= "{}/cap_cam_{}.jpg".format('./test/get_data', img_counter) #Image Name
+        cv.imwrite(img_name, frame)
+        print("Data Taken")
+        img_counter+=1
+    cam.release() #Release Camera
+    cam.destroyAllWindows() #Close All Opened Windows
 
 # Parser(ImagePath)
-# print(int_img[1])
+
+# for image in int_img:
+#     # image_int_data= imread(image)
+#     cv.imshow('Image', image)
+#     cv.waitKey(0)
+# cv.destroyAllWindows()
